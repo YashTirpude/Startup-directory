27b462be45e2b4bc9dea30d3403401855ca2b184
Yash <137068938+YashTirpude@users.noreply.github.com>
2024-12-11 18:11:57 +0530

    hero section added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 
-const page = () => {
+const Home = () => {
   return (
     <>
-      <h1 className="tsx-2xl">Home</h1>
+      <section className="blue_container ">
+        <h1 className="heading">
+          Pitch Your Startup,
+          <br />
+          Connect With Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in virtual Competitions
+        </p>
+      </section>
     </>
   );
 };
 
-export default page;
+export default Home;
