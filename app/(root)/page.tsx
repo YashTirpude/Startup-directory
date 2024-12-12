114337d7fc1a9fb165daa09b802353301fd0b059
Yash <137068938+YashTirpude@users.noreply.github.com>
2024-12-12 17:18:28 +0530

    added search by query

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,41 +1,35 @@
-import React from "react";
-import SearchForm from "../../components/SearchForm";
-import StartupCard from "@/components/StartupCard";
+import SearchForm from "@/components/SearchForm";
 
-const Home = async ({
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
+
+export default async function Home({
   searchParams,
 }: {
   searchParams: Promise<{ query?: string }>;
-}) => {
+}) {
   const query = (await searchParams).query;
+  const params = { search: query || null };
 
-  const posts = [
-    {
-      _createdAt: new Date(),
-      views: 55,
-      author: { _id: 1, name: "Azrael" },
-      _id: 1,
-      description: "This is a description",
-      image:
-        "https://th.bing.com/th/id/R.2436bfd57f8da9d3352be6d9c69f7d0d?rik=9ZCsDkOmNhWhyQ&riu=http%3a%2f%2fwallpaperheart.com%2fwp-content%2fuploads%2f2018%2f04%2fbest-scenery-wallpaper-scenery-images.jpg&ehk=QhEgBXlBIdu4NBBKeTvxOQncAMHrLokkC8ustgaeB9c%3d&risl=&pid=ImgRaw&r=0",
-      category: "Robots",
-      title: "Robot",
-    },
-  ];
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
 
   return (
     <>
-      <section className="blue_container ">
+      <section className="blue_container">
         <h1 className="heading">
-          Pitch Your Startup,
-          <br />
+          Pitch Your Startup, <br />
           Connect With Entrepreneurs
         </h1>
+
         <p className="sub-heading !max-w-3xl">
-          Submit Ideas, Vote on Pitches, and Get Noticed in virtual Competitions
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
         </p>
+
         <SearchForm query={query} />
       </section>
+
       <section className="section_container">
         <p className="text-30-semibold">
           {query ? `Search results for "${query}"` : "All Startups"}
@@ -43,16 +37,16 @@ const Home = async ({
 
         <ul className="mt-7 card_grid">
           {posts?.length > 0 ? (
-            posts.map((post: StartupCardType) => (
-              <StartupCard key={post._id} post={post} />
+            posts.map((post: StartupTypeCard) => (
+              <StartupCard key={post?._id} post={post} />
             ))
           ) : (
             <p className="no-results">No startups found</p>
           )}
         </ul>
       </section>
+
+      <SanityLive />
     </>
   );
-};
-
-export default Home;
+}
--- sanity.config.ts
+++ sanity.config.ts
@@ -4,11 +4,12 @@
  * This configuration is used to for the Sanity Studio that’s mounted on the `\app\studio\[[...tool]]\page.tsx` route
  */
 
-import { visionTool } from "@sanity/vision";
+// import {visionTool} from "@sanity/vision"
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
+import { visionTool } from "@sanity/vision";
 import { apiVersion, dataset, projectId } from "./sanity/env";
 import { schema } from "./sanity/schemaTypes";
 import { structure } from "./sanity/structure";
