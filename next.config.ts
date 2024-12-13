0643ab151345e9c0e806d539f81abb9299278f99
Yash <137068938+YashTirpude@users.noreply.github.com>
2024-12-13 17:13:32 +0530

    next.config updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
+import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     dangerouslyAllowSVG: true,
     remotePatterns: [
@@ -11,6 +17,32 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  experimental: {
+    ppr: "incremental",
+    after: true,
+  },
+  devIndicators: {
+    appIsrStatus: true,
+    buildActivity: true,
+    buildActivityPosition: "bottom-right",
+  },
 };
 
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+  org: "js-mastery-dk",
+  project: "javascript-nextjs",
+
+  silent: !process.env.CI,
+
+  widenClientFileUpload: true,
+
+  reactComponentAnnotation: {
+    enabled: true,
+  },
+
+  hideSourceMaps: true,
+
+  disableLogger: true,
+
+  automaticVercelMonitors: true,
+});
